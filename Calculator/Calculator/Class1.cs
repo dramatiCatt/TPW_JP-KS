bc34722269f23b5e390c47acb2898e5f3211c5a5
dramatiCatt <117084758+dramatiCatt@users.noreply.github.com>
2023-03-11 20:13:29 +0100

    Add files via upload

---
+++ Calculator/Calculator/Class1.cs
@@ -0,0 +1,25 @@
+﻿namespace Calculator
+{
+    public class Calc
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hejka, tu nasz kalkulator");
+        }
+
+        public int sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int mul(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int div(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}

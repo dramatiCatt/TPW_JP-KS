983b2835a24bbd5e9775986064fb7d90fca11055
PetraNovva <jula111002@gmail.com>
2023-04-14 12:25:35 +0200

    The beggining of the project
    Deleted caltulator, added base for project, only 1 solution.

---
+++ Data/DataAbstractApi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data
+{
+    public abstract class DataAbstractApi
+    {
+        public static DataAbstractApi CreateBallData()
+        {
+            return new BallData();
+        }
+        
+    }
+    internal class BallData : DataAbstractApi
+    {
+        //Nothing
+    }
+
+}

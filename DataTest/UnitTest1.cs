cba1fba2448903fe648e45e82441ce4031ac614a
dramatiCatt <karolinasoltysiak11@gmail.com>
2023-05-21 21:53:35 +0200

    it works!!!! tbd: tests

--- DataTest/UnitTest1.cs
+++ DataTest/UnitTest1.cs
@@ -17,8 +17,8 @@ public class Tests
     {
         Creator creator = new Creator();
         Ball ball = creator.CreateBall();
-        Assert.True(ball.currentVector.X >= 2 + creator.Radius && ball.currentVector.X <= Manager.width - creator.Radius - 1);
-        Assert.True(ball.currentVector.Y >= 2 + creator.Radius && ball.currentVector.Y <= Manager.height - creator.Radius - 1);
+        Assert.True(ball.CurrentVector.X >= 2 + creator.Radius && ball.CurrentVector.X <= Manager._width - creator.Radius - 1);
+        Assert.True(ball.CurrentVector.Y >= 2 + creator.Radius && ball.CurrentVector.Y <= Manager._height - creator.Radius - 1);
         //Assert.True(ball.Weight <= 2 && ball.Weight > 0);
         Assert.AreEqual(2, ball.Weight);
     }
